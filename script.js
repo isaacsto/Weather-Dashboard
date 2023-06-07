c15643477661c5395185301ce2f42f1ad0536055
Isaac Stofko <isaacstofko@gmail.com>
2023-06-07 14:49:58 -0400

    createCard function

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ console.log(dateString);
 
 
 //makes searchBtn clickable, puts value of user input into variable, alerts if invalid value is input 
-document.querySelector("#searchBtn").addEventListener("click", function () {
-    e.preventDefault();
+document.getElementById("searchBtn").addEventListener("click", function () {
+    event.preventDefault();
 
-    var cityInput = document.getElementById("#cityInput").value;
+    var cityInput = document.getElementById("cityInput").value;
     /* var searchInput = document.getElementById('cityInput'); */
    /*  var searchValue = searchInput.value; */
 
@@ -25,7 +25,8 @@ document.querySelector("#searchBtn").addEventListener("click", function () {
     }
 
     //push city value to cityInput el
-    cityInput.push(city);
+   searchHistory.push(cityInput);
+   console.log(searchHistory)
 
     //save to localstorage 
     localStorage.setItem('city', JSON.stringify(searchHistory))
@@ -34,13 +35,14 @@ document.querySelector("#searchBtn").addEventListener("click", function () {
 });
 
 //create buttons based on search history via js 
-var createHistoryEl = document.getElementById(cityHistory);
+var createHistoryEl = document.createElement("div");
+
 function displayHistory() {
-    createHistoryEl.empty();
+    event.preventDefault();
 
 for (let i = 0; i < searchHistory.length; i++) {
     var rowElement = $('<row>');
-    var buttonElement = $('<button').text('${searchHist[i]}')
+    var buttonElement = $('<button').text('${searchHistory[i]}')
 
     buttonElement.attr('type', 'button');
     buttonElement.addClass('btn btn-outline-secondary historyButton')
@@ -72,16 +74,16 @@ function getTodayWeath(){
     })
     .then(function(data) {
       $('.todayCityName').text(data.name);
-      $('.todayCard').text(date);
+      $('.todayCard').text(currentDate);
       $('.icons').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
       var pEl = $('<p>').text(`Temperature: ${data.main.temp} °F`);
-      cardTodayBody.append(pEl);
+      todayCard.append(pEl);
       var pElTemp = $('<p>').text(`Feels Like: ${data.main.feels_like} °F`);
-      cardTodayBody.append(pElTemp);
+      todayCard.append(pElTemp);
       var pElHumid = $('<p>').text(`Humidity: ${data.main.humidity} %`);
-      cardTodayBody.append(pElHumid);
+      todayCard.append(pElHumid);
       var pElWind = $('<p>').text(`Wind Speed: ${data.wind.speed} MPH`);
-      cardTodayBody.append(pElWind);
+      todayCard.append(pElWind);
 
       var cityLong = data.coord.lon;
       var cityLat = data.coord.lat;
@@ -90,131 +92,76 @@ function getTodayWeath(){
       console.log(error);
     });
 
+    getFiveDayForecast();
+
 };
 
 
-//passes city as argument function that hold both current and 5 day forecast fetches 
-function callFetch(city) {
-    //gets date for display
-    var currentDate = new Date();
-    var month = currentDate.getMonth() + 1;
-    var day = currentDate.getDate();
-    var year = currentDate.getFullYear();
-    var fullDate = month + '/' + day + '/' + year;
+var putFiveDayForeCast = $('.fiveDayForeCast')   
 
-    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-    // fetches queryUrl converts response to json then selects weatherInfo div 
-    fetch(currentWeatherUrl)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            var weatherInfoDiv = document.querySelector("#weather-info");
-            //classes for css
-            weatherInfoDiv.innerHTML = `<h2 class="weather-head">${data.name} ${fullDate}</h2><br> <p class="dat">Temperature: ${data.main.temp} °F</p><br> <p class="dat">Wind Speed: ${data.wind.speed} mph</p><br> <p class="dat">Humidity: ${data.main.humidity}% </p>`
+var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`
 
-            //Add city to search history when search button is clicked
-        })
-
-    
-    var fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`
-
-    //fetches 5 day forecast`
+function getFiveDayForecast () {
     fetch(fiveDayForecast)
-        .then(response => response.json())
-        .then(data => {
-            //pushes searched items to searchHistory var 
-            searchHistory.push(data.city.name);
-            console.log(searchHistory)
-            displayHistory();
-            //makes variables arrays so data can more easily be extracted for the display
-            var temp = []
-            var fiveDayHumidity = []
-            var fiveDayWind = []
-            console.log(data)
-            // for loop iterates through data array so only 5 temperatures show up  
-            for (i = 0; i < 40; i += 8) {
-                console.log(data.list[i].main.temp)
-                temp.push(data.list[i].main.temp);
-            }
-            //for loop to iterate through data array and display humidity
-            for (i = 0; i < 40; i += 8) {
-                console.log(data.list[i].main.humidity)
-                fiveDayHumidity.push(data.list[i].main.humidity);
-            }
-            //for loop to iterate through data array and displays wind speed
-            for (i = 0; i < 40; i += 8) {
-                console.log(data.list[i].wind.speed)
-                fiveDayWind.push(data.list[i].wind.speed)
-            }
-
-            // get dates for fiveDayForecast
-            var newDates = []
-            for (var i = 1; i <= 5; i++) {
-                var nextDate = new Date(currentDate.getTime() + i * 24 * 60 * 60 * 1000);
-
-                var day = nextDate.getDate();
-                var month = nextDate.getMonth() + 1;
-                var year = nextDate.getFullYear();
-
-                var dateString = `${month}/${day}/${year}`
-                console.log(dateString)
-                //pushes dateString to a var so I can pull each index for each forecast element
-                newDates.push(dateString)
-            }
-
-           //cleardiv 
-            dayOneDiv.innerHTML = ''; 
-           
-            dayTwoDiv.innerHTML = '';
-        
-            dayThreeDiv.innerHTML = '';
-            
-            dayFourDiv.innerHTML = '';
-            
-            dayFiveDiv.innerHTML = '';
-            
-
-
-            //concatenate values of each data variable
-            
-            for (i = 0; i < 5; i++) {
-                var dayDiv = document.createElement("div");
-                dayDiv.classList.add('col', 's12','m6', '12');
-                
-                var dayDate = newDates[i];
-                var dayTemp = temp[i];
-                var dayHumidity = fiveDayHumidity[i];
-                var dayWind = fiveDayWind[i]
-
-                //add content 
-                dayDiv.innerHTML = `
-                <p>${dayDate}</p>
-                <p>Temp: ${dayTemp} °F</p>
-                <p>Humidity: ${dayHumidity} %</p>
-                <p>Wind-speed: ${dayWind} mph</p>`
-                
-            }
-
-            //append new div element to corresponding day div 
-
-            if (i === 0) {
-                dayOneDiv.appendChild(dayDiv);
-            } else if (i === 1) {
-                dayTwoDiv.appendChild(dayDiv);
-            } else if (i === 2) {
-                dayThreeDiv.appendChild(dayDiv);
-            } else if (i === 3) {
-                dayFourDiv.appendChild(dayDiv);
-            } else if (i === 4) {
-                dayFiveDiv.appendChild(dayDiv)
-            }
-            
-        })
-        .catch(error => {
-            console.error("An error occured while fetching the data:", error);
-        })
-
-}
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            var fiveDayArray = data.list;
+            var myWeather = [];
+      
+            fiveDayArray.forEach(function(value) {
+              var testObj = {
+                date: value.dt_txt.split(' ')[0],
+                time: value.dt_txt.split(' ')[1],
+                temp: value.main.temp,
+                feels_like: value.main.feels_like,
+                icon: value.weather[0].icon,
+                humidity: value.main.humidity
+              };
+      
+              if (value.dt_txt.split(' ')[1] === '12:00:00') {
+                myWeather.push(testObj);
+              }
+            });
+      
+            var fiveForecastEl = $('.fiveForecast'); // Assuming you have an element with the class 'fiveForecast' to append the cards
+      
+            myWeather.forEach(function(weather) {
+              var divElCard = $('<div>');
+              divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
+              divElCard.attr('style', 'max-width: 200px;');
+              fiveForecastEl.append(divElCard);
+      
+              var divElHeader = $('<div>');
+              divElHeader.attr('class', 'card-header');
+              var m = moment(weather.date).format('MM-DD-YYYY');
+              divElHeader.text(m);
+              divElCard.append(divElHeader);
+      
+              var divElBody = $('<div>');
+              divElBody.attr('class', 'card-body');
+              divElCard.append(divElBody);
+      
+              var divElIcon = $('<img>');
+              divElIcon.attr('class', 'icons');
+              divElIcon.attr('src', `https://openweathermap.org/img/wn/${weather.icon}@2x.png`);
+              divElBody.append(divElIcon);
+      
+              var pElTemp = $('<p>').text(`Temperature: ${weather.temp} °F`);
+              divElBody.append(pElTemp);
+      
+              var pElFeel = $('<p>').text(`Feels Like: ${weather.feels_like} °F`);
+              divElBody.append(pElFeel);
+      
+              var pElHumid = $('<p>').text(`Humidity: ${weather.humidity} %`);
+              divElBody.append(pElHumid);
+            });
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      };     
 
 function createCard(data) {
     var card = document.createElement('div');
@@ -244,50 +191,18 @@ function createCard(data) {
     var container = dcoument.querySelector('.card-container')
     container.appendChild(card);
 
-}
+};
 
-//functions so css properties don't show up until the search button is pressed 
-function displayDays() {
-    var container = document.getElementById("card-container");
-    container.classList.add("displayed")
-}
-function displayCurrent() {
-    var currentContainer = document.getElementById("current-container");
-    currentContainer.classList.add("displayed")
-} 
+function onLoad() {
 
-   /*  var searchHistory = JSON.parse(localStorage.getItem("searchHistory"))  */
-    
-/*     if (!Array.isArray(searchHistory)) {
-        searchHistory = []
-    } */
+	var cityHist = JSON.parse(localStorage.getItem('city'));
 
-   /*  searchHistory.push(searchValue),
-    
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory)) ;
+	if (cityHist !== null) {
+		searchHistory = cityHist
+	}
+	displayHistory();
+	getTodayWeath();
+};
 
-    callFetch(cityInput);
-
-    //css display functions 
-    displayCurrent()
-    displayDays()
-
-})
-
-//call displayHistory on load 
-window.onload = displayHistory()
-
-function displayHistory (searchValue) {
-    localStorage.getItem("searchHistory", JSON.stringify(searchValue))
-    var historyContainer = document.querySelector('#searchHistory')
-        if (searchHistory.length > 0) {
-            searchHistory.forEach(searchValue => {
-                var aButton = document.createElement('button');
-                aButton.textContent = searchValue;
-                historyContainer.appendChild(aButton)
-            })
-        }
-    }
-    
- */
+onLoad();
 
