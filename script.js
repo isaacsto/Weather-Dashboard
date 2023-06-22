9b81841bf432e02ff4184445dcb6421e01b51212
Isaac Stofko <isaacstofko@gmail.com>
2023-06-22 17:08:08 -0400

    reset

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ histButton.addEventListener('click', displayHistory);
 
 
 function getTodayWeath(){
-    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + encodeURIComponent(city) + "&units=imperial&appid=" + APIKey;
 
     $('.todayBody').empty();
 
@@ -85,113 +85,13 @@ function getTodayWeath(){
       console.log(error);
     });
 
-    getFiveDayForecast();
 
 };
 
 
-var putFiveDayForeCast = $('.fiveDayForeCast')   
 
 
-var fiveDayForecast = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-
-function getFiveDayForecast () {
-    fetch(fiveDayForecast)
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(data) {
-            var fiveDayArray = data.list;
-            var myWeather = [];
-      
-            fiveDayArray.forEach(function(value) {
-              var testObj = {
-                date: value.dt_txt.split(' ')[0],
-                time: value.dt_txt.split(' ')[1],
-                temp: value.main.temp,
-                feels_like: value.main.feels_like,
-                icon: value.weather[0].icon,
-                humidity: value.main.humidity
-              };
-      
-              if (value.dt_txt.split(' ')[1] === '12:00:00') {
-                myWeather.push(testObj);
-              }
-            }); 
-
-            for (var i = 0; i < 5; i++) {
-              var forecast = data.list[i]
-            }
-       
-            var fiveForecastEl = $('.row fiveForecast'); 
-
-            myWeather.forEach(function(weather) {
-              var divElCard = $('<div>');
-              divElCard.attr('class', 'card text-white bg-primary mb-3 cardOne');
-              divElCard.attr('style', 'max-width: 200px;');
-              fiveForecastEl.append(divElCard);
-      
-              var divElHeader = $('<div>');
-              divElHeader.attr('class', 'card-header');
-              var m = moment(weather.date).format('MM-DD-YYYY');
-              divElHeader.text(m);
-              divElCard.append(divElHeader);
-      
-              var divElBody = $('<div>');
-              divElBody.attr('class', 'card-body');
-              divElCard.append(divElBody);
-      
-              var divElIcon = $('<img>');
-              divElIcon.attr('class', 'icons');
-              divElIcon.attr('src', `https://openweathermap.org/img/wn/${weather.icon}@2x.png`);
-              divElBody.append(divElIcon);
-      
-              var pElTemp = $('<p>').text(`Temperature: ${weather.temp} °F`);
-              divElBody.append(pElTemp);
-      
-              var pElFeel = $('<p>').text(`Feels Like: ${weather.feels_like} °F`);
-              divElBody.append(pElFeel);
-      
-              var pElHumid = $('<p>').text(`Humidity: ${weather.humidity} %`);
-              divElBody.append(pElHumid);
-            });
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
-      };     
-
-function createCard(data) {
-    var card = document.createElement('div');
-    card.classList.add('card')
-
-    var cardBody = document.createElement('div')
-    cardBody.classList.add('card-body');
-
-    var city = document.createElement('h5');
-    city.classList.add('card-title');
-    city.textContent = data.name;
-
-    var temp = document.createElement('p');
-    temp.classList.add('card-text');
-    temp.textContent = 'Temperature ' + data.main.temp + '°F';
-
-    var humidity = document.createElement('p');
-    humidity.classList.add('card-text');
-    humidity.textContent = 'Humidity: ' + data.main.humidity + '%'
-
-    cardBody.appendChild(city);
-    cardBody.appendChild(temp);
-    cardBody.appendChild(humidity);
-
-    card.appendChild(cardBody)
-
-    var container = dcoument.querySelector('.card-container')
-    container.appendChild(card);
-
-};
-
-function onLoad() {
+/* function onLoad() {
 
 	var cityHist = JSON.parse(localStorage.getItem('city'));
 
@@ -203,4 +103,4 @@ function onLoad() {
 };
 
 onLoad();
-
+ */
